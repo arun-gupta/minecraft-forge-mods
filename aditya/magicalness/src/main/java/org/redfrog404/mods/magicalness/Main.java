ec590911482d2ec6fc7e7f0cdd8e7364230a7ffc
BuildTools <unconfigured@null.spigotmc.org>
2015-04-01 21:50:39 -0700

    Updated Magicalness to version 1.0.1

--- aditya/magicalness/src/main/java/org/redfrog404/mods/magicalness/Main.java
+++ aditya/magicalness/src/main/java/org/redfrog404/mods/magicalness/Main.java
@@ -16,7 +16,7 @@ import net.minecraftforge.fml.common.registry.GameRegistry;
 @Mod(modid = Main.MODID, version = Main.VERSION)
 public class Main {
 	public static final String MODID = "Magicalness";
-	public static final String VERSION = "1.0";
+	public static final String VERSION = "1.0.1";
 
 	// Materials
 
